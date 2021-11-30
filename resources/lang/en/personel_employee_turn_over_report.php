6fc01c48aebf6c32e5bf2a2059f43f06c1cdc1b9
Farhan <farhand226@gmail.com>
2021-11-30 22:46:38 +0700

    Update View Personel Employee

---
+++ resources/lang/en/personel_employee_turn_over_report.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Personel - Employee List Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Pengaturan Halaman Personel - Employee List Menggunakan Locale Bahasa Inggris. Akses di file
+    | personel_employee_list.blade.php
+    |
+    */
+
+    'judul' => 'Employee Turnover',
+    'list' => 'Personel Menu List',
+    'label_employee_no_from' => 'Employee No From',
+    'label_employee_no_to' => 'Employee No To',
+    'label_evaluation_period_from' => 'Evaluation Period From',
+    'label_evaluation_period_to' => 'Evaluation Period To',
+    'label_group_authorize_from' => 'Group Authorize From',
+    'label_group_authorize_to' => 'Group Authorize To',
+    'label_position' => 'Position',
+    'label_ranking' => 'Ranking',
+    'label_location' => 'Location',
+    'label_level_1' => '(Level 1 Code)',
+    'label_level_2' => '(Level 2 Code)',
+    'label_level_3' => '(Level 3 Code)',
+    'btn_print' => 'Print'
+
+];
