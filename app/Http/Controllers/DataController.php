0d3a4d3c3364f5b49ecbca07ab4becec6d6a23a8
Yafie Imam <yafie.achmad@intikom.co.id>
2023-11-27 09:39:26 +0700

    Update Input Balance Leave

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -1912,21 +1912,25 @@ class DataController extends Controller
 
 		// $arrData = unique_multidim_array($arrResult->dataListSet, 'employeeNo');
 
-		// var_dump($arrResult->dataListSet->pluck('employeeNo'));
+		// dd($arrResult->dataListSet);
 
 	    if($search == ''){
 	    	$data = $arrResult->dataListSet;
 	    }else{
-	    	$data = array_filter(
+	    	$filteredData = array_filter(
 	    		$arrResult->dataListSet,
 	    		function($value) use ($search){
 	    			if(preg_match('/' . $search . '/i', $value->employeeNo)){
 	    				return preg_match('/' . $search . '/i', $value->employeeNo);
+	    			} else if(preg_match('/' . $search . '/i', $value->employeeName)){
+	    				return preg_match('/' . $search . '/i', $value->employeeName);
 	    			}
 	    		}
 	    	);
+
+			$data = array_values($filteredData);
 	    }
-		// var_dump($arrResult);
+		// dd($data);
         return response()->json($data);
 	}
 
