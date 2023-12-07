ae8eac7547eaf14427cff7f3eeaeaeca90b0569a
INTIKOM\nadirrb <nadir.bakri@intikom.co.id>
2023-12-07 15:59:36 +0700

    import export

--- app/Exports/TemplatePersonalDataTemplateSheet.php
+++ app/Exports/TemplatePersonalDataTemplateSheet.php
@@ -5,13 +5,48 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Validator;
+use Session;
+use App;
 
 class TemplatePersonalDataTemplateSheet implements FromView, WithTitle, ShouldAutoSize
 {
     public function view(): View
     {
-        return view('personel.personel_export_template_personal_data_template_sheet');
+        try {
+            $client = new Client([
+                'verify' => false,
+                'headers' => [ 'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . Session::get('token') ]
+            ]);
+
+            $response = $client->post(env('API_URL') . '/PeMaster/getTemplate',
+                ['body' => json_encode(
+                    [
+                        'companyCode' => Session::get('companyCode')
+                    ]
+                )]
+            );
+
+            $arrResult = json_decode($response->getBody()->getContents());
+
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+            if($response->getStatusCode() == 401){
+                return view('error.login');
+            }else if($response->getStatusCode() == 404){
+                return view('error.not_found');
+            }else{
+                return view('error.bad_request');
+            }
+        }
+            
+        return view('personel.personel_export_template_personal_data_template_sheet', [
+            'data' => ($arrResult != null) ? $arrResult : []
+        ]);
     }
 
     public function title(): string
