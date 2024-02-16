431e1215e219b1de7bc440049ccef38bb7372b71
INTIKOM\nadirrb <nadir.bakri@intikom.co.id>
2024-02-16 09:43:57 +0700

    transfer to bank (format lama)

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -4397,21 +4397,21 @@ public function dataDetailReportFormatPY(Request $request)
         // exit;
 
         if($arrResult->dataListSet != null){
-            if($request->output_file == 'BANK CENTRAL ASIA'){
+            if($request->source_bank == 'BANK CENTRAL ASIA'){
                 $fullPath = storage_path('app/');
                 array_map('unlink', glob( "$fullPath*.txt"));
                 $arrDataBCA = explode("\r\n", $arrResult->dataListSet[0]->transferBank);
                 Storage::put($arrResult->dataListSet[0]->namaFile, $arrResult->dataListSet[0]->transferBank);
                 return Storage::download($arrResult->dataListSet[0]->namaFile);
-            }else if($request->output_file == 'MCM'){
+            }else if($request->source_bank == 'MCM'){
                 return Excel::download(new CSVTransferBankMCMExport($arrResult->dataListSet[0]->transferBank), $arrResult->dataListSet[0]->namaFile);
-            }else if($request->output_file == 'BOT'){
+            }else if($request->source_bank == 'BOT'){
                 return Excel::download(new CSVTransferBankBOTExport($arrResult->dataListSet[0]->transferBank), $arrResult->dataListSet[0]->namaFile);
-            }else if($request->output_file == 'BTPN'){
+            }else if($request->source_bank == 'BTPN'){
                 return Excel::download(new CSVTransferBankBTPNExport($arrResult->dataListSet[0]->transferBank), $arrResult->dataListSet[0]->namaFile);
-            }else if($request->output_file == 'BANK INA' || $request->output_file == 'BANK INA MULTI ACCOUNT'){
+            }else if($request->source_bank == 'BANK INA' || $request->source_bank == 'BANK INA MULTI ACCOUNT'){
                 return Excel::download(new CSVTransferBankINAExport($arrResult->dataListSet[0]->transferBank), $arrResult->dataListSet[0]->namaFile);
-            }else if($request->output_file == 'BNI'){
+            }else if($request->source_bank == 'BNI'){
                 return Excel::download(new CSVTransferBankBNIExport($arrResult->dataListSet[0]->transferBank), $arrResult->dataListSet[0]->namaFile);
             }
         }
