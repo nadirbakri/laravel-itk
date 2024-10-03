ebef4dffe80bea114e46d72284ac5d6f53510ec5
Audrey Zahra <audreyzahra2@gmail.com>
2024-10-03 20:50:10 +0700

    Fix Export Business Trip PDF

--- resources/views/export/exp_businesstrippdf_list2.blade.php
+++ resources/views/export/exp_businesstrippdf_list2.blade.php
@@ -28,95 +28,97 @@
         @foreach($data as $index => $item)
         <table style="width:100%; @if ($index < count($data) - 1) page-break-after: always; @endif">
             <tr>
-                <td width='50%' style="font-size: 18px; font-weight: 700; text-align: left;">{{ $item->companyName }}</td>
-                <td width='50%' style="font-size: 14px; text-align: right;">Date : {{ date('d-M-Y', strtotime($item->createdDate)) }} | Assesment : {{ $item->costCenterCode }}</td>
+                <td width='20%' style="font-size: 18px; font-weight: 700; text-align: left;">{{ $item->companyName }}</td>
+                <td>&nbsp;</td>
+                <td width='80%' style="font-size: 14px; text-align: right;">Date : {{ date('d-M-Y', strtotime($item->createdDate)) }} | Assesment : {{ $item->costCenterCode }}</td>
             </tr>
             <tr>
-                <td colspan="2"><hr style="margin-top: 20px; margin-bottom: 20px;" /></td>
+                <td colspan="3"><hr style="margin-top: 20px; margin-bottom: 20px;" /></td>
             </tr>
             <tr>
-                <td colspan="2" style="font-size: 18px; font-weight: 700; text-align: center;">TRAVEL ADVANCE PAYMENT REQUEST</td>
+                <td colspan="3" style="font-size: 18px; font-weight: 700; text-align: center;">TRAVEL ADVANCE PAYMENT REQUEST</td>
             </tr>
             <br>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Status</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->status }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Status</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->status }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Doc No.</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->ticketNo }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Doc No.</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->ticketNo }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Name</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->fullName }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Name</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->fullName }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Division</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->costCenterCode }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Division</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->costCenterCode }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>SPPD Type</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->kategoriDestinasi }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>SPPD Type</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->kategoriDestinasi }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Destination</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->destination }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Destination</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->destination }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Travel Period</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ date('d-m-Y', strtotime($item->startDate)) }} until {{ date('d-m-Y', strtotime($item->endDate)) }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Travel Period</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ date('d-m-Y', strtotime($item->startDate)) }} until {{ date('d-m-Y', strtotime($item->endDate)) }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Total Days</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->totalDays }} days</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Total Days</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->totalDays }} days</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Purpose</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->purpose }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Purpose</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->purpose }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Project</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->projectName }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Project</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->projectName }}</span></td>
             </tr>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Customer</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->customerName }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Customer</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->customerName }}</span></td>
             </tr>
             <tr>
-                <td colspan="2"><hr style="margin-top: 20px; margin-bottom: 20px;" /></td>
+                <td colspan="3"><hr style="margin-top: 20px; margin-bottom: 20px;" /></td>
             </tr>
             <tr>
                 <td width='50%' style="font-size: 18px; font-weight: 700; text-align: left; padding-bottom: 10px;">ESTIMATED EXPENSES</td>
             </tr>
             <br>
             <tr>
-                <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Currency</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->currency }}</span></td>
+                <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Currency</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>{{ $item->currency }}</span></td>
             </tr>
             @foreach($item->claimList as $claim)
                 {{-- @if($claim->claimName == 'Pocket Money') --}}
                     <tr>
-                        <td width='17%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
+                        <td width='20%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
                             <span>{{ $claim->claimName }}</span>
                         </td>
+                        <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
                         @if($claim->claimPerDay !== $claim->totalClaimPerDay)
-                            <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
+                            <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
                                 <span>Rp. {{ $claim->claimPerDay }} / Rp. {{ $claim->totalClaimPerDay }}</span>
                             </td>
                         @else
-                            <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
+                            <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;">
                                 <span>Rp. {{ $claim->totalClaimPerDay }}</span>
                             </td>
                         @endif
@@ -124,9 +126,9 @@
                 {{-- @endif --}}
             @endforeach
             <tr>
-                <td width='17%' style="font-size: 14px; font-weight: 700; text-align: left; padding-bottom: 10px;"><span>TOTAL</span></td>
-                {{-- <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td> --}}
-                <td width='80%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Rp. {{ $item->totalClaimAmount }}</span></td>
+                <td width='20%' style="font-size: 14px; font-weight: 700; text-align: left; padding-bottom: 10px;"><span>TOTAL</span></td>
+                <td width='3%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>:</span></td>
+                <td width='77%' style="font-size: 14px; text-align: left; padding-bottom: 10px;"><span>Rp. {{ $item->totalClaimAmount }}</span></td>
             </tr>
         </table>
         @endforeach
