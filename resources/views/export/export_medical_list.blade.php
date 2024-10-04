5a42fd65d73521381f5208e9dd750ad8f9a03502
Yafie Imam <yafie.achmad@intikom.co.id>
2024-10-04 08:35:00 +0700

    Update Export Medical

--- resources/views/export/export_medical_list.blade.php
+++ resources/views/export/export_medical_list.blade.php
@@ -23,17 +23,19 @@
 				<th>Request Date</th>
 				<th>Status</th>
 				<th>Ticket Number</th>
-				<th>Claim Type</th>
-				<th>Employee No</th>
+				<th>Employee Name</th>
+				<th>Division</th>
 				<th>Receipt Date</th>
-				{{-- <th>Company Customer</th> --}}
+				<th>Claim For</th>
+				<th>Dependent Name</th>
 				<th>Remarks</th>
-				{{-- <th>Total Request(Rp)</th>
-				<th>Total Per Employee</th>
+				<th>Total Request (Rp)</th>
+				<th>Total Per Employee (Rp)</th>
 				<th>No Rekening</th>
-				<th>Total Approved HRD(Rp)</th> --}}
-				<th>Total Paid</th>
+				<th>Total Approved HRD (Rp)</th>
+				<th>Total Paid (Rp)</th>
 				<th>Paid Remarks</th>
+				<th>Alamat Email</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -41,22 +43,22 @@
 			@foreach($data as $value)
 			<tr>
                 <td>{{ $no++ }}</td>
-				<td>{{ \Carbon\Carbon::parse($value->reimbursementEntity->createdDate)->format('Y-m-d') }}</td>
-				<td>{{ $value->reimbursementEntity->reimbursementStatus }}</td>
-				<td>{{ $value->reimbursementEntity->ticketNo }}</td>
-				<td>{{ $value->reimbursementEntity->medicalType1 }}</td>
-				<td>{{ $value->reimbursementEntity->employeeNo}}</td>
-				<td>{{ \Carbon\Carbon::parse($value->reimbursementEntity->receiptDate)->format('Y-m-d') }}</td>
-				{{-- <td><b>TOTAL</b></td> --}}
-				<td>{{ $value->reimbursementEntity->reimbursementRemarks}}</td>
-				{{-- <td><b>0</b></td>
-				<td><b>0</b></td>
-				<td>#</td>
-				<td></td> --}}
-				<td>{{ $value->reimbursementEntity->paidAmount}}</td>
-				<td>{{ $value->reimbursementEntity->approvalRemarks}}</td>
-				{{-- <td>{{ $value->reimbursementEntity->paymentDate}}</td> --}}
-				
+				<td>{{ isset($value->reimbursementEntity->createdDate) ? \Carbon\Carbon::parse($value->reimbursementEntity->createdDate)->format('Y-m-d') }} : ''</td>
+				<td>{{ isset($value->reimbursementEntity->reimbursementStatus) ? $value->reimbursementEntity->reimbursementStatus : '' }}</td>
+				<td>{{ isset($value->reimbursementEntity->ticketNo) ? $value->reimbursementEntity->ticketNo : '' }}</td>
+				<td>{{ isset($value->reimbursementEntity->fullnameRequester) ? $value->reimbursementEntity->fullnameRequester : '' }}</td>
+				<td>{{ isset($value->reimbursementEntity->businessUnit) ? $value->reimbursementEntity->businessUnit : '' }}</td>
+				<td>{{ isset($value->reimbursementEntity->receiptDate) ? \Carbon\Carbon::parse($value->reimbursementEntity->receiptDate)->format('Y-m-d') }}</td>
+				<td>{{ isset($value->reimbursementEntity->claimFor) ? $value->reimbursementEntity->claimFor }}</td>
+				<td>{{ isset($value->reimbursementEntity->dependentName) ? $value->reimbursementEntity->dependentName }}</td>
+				<td>{{ isset($value->reimbursementEntity->reimbursementRemarks) ? $value->reimbursementEntity->reimbursementRemarks }}</td>
+				<td>{{ isset($value->reimbursementEntity->totalClaimAmount) ? $value->reimbursementEntity->totalClaimAmount }}</td>
+				<td>{{ isset($value->reimbursementEntity->totalClaimAmountPerEmployee) ? $value->reimbursementEntity->totalClaimAmountPerEmployee }}</td>
+				<td>{{ isset($value->reimbursementEntity->bankAccountNo) ? $value->reimbursementEntity->bankAccountNo }}</td>
+				<td></td>
+				<td>{{ isset($value->reimbursementEntity->paidAmount) ? $value->reimbursementEntity->paidAmount }}</td>
+				<td>{{ isset($value->reimbursementEntity->approvalRemarks) ? $value->reimbursementEntity->approvalRemarks }}</td>
+				<td>{{ isset($value->reimbursementEntity->emailRequester) ? $value->reimbursementEntity->emailRequester }}</td>
 			</tr>
 			@endforeach
 		</tbody>
