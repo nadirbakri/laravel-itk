407d24ae1937d83e00dec0eb36046dd0d0e3fe2d
Yafie Imam <yafie.achmad@intikom.co.id>
2024-11-18 09:26:50 +0700

    Update Penyesuaian Periodical Report

--- resources/views/payroll/py_export_periodical_report.blade.php
+++ resources/views/payroll/py_export_periodical_report.blade.php
@@ -34,94 +34,12 @@
 	</style>
 </head>
 <body>
-    <h3>{{ $data_company[0]->companyName }} <br> {{ $data_company[0]->address }}</h3>
-    <h3 style="text-align:center">{{ $report_name }}</h3>
-    <h4 style="text-align:center">Period : {{ date('F Y', strtotime($data_period)) }}</h4>
-    <?php
-    $total = [];
-    ?>
-    @if(count($data) > 0 && (count($data[0]->detail) > 0 || count($data[0]->summary) > 0))
-    <?php
-        $countcolspan = count($data[0]->detail[0]->field) + 3;
-    ?>
-    <table style="width: 100%;" class="table table-bordered table-hover responsive table_detail">
-        <thead>
-            <tr>
-                <th style="text-align:center; align-items:center; border:1px solid #000; padding:4px; background-color: #97d7f7;">No</th>
-                @foreach($data[0]->detail[0]->field as $key => $dataTable)
-                    @if(!is_string($dataTable->value) && ($dataTable->dataFormat == "#,##0" || $dataTable->dataFormat == "#,##0.00"))
-                        <?php
-                        $total[$dataTable->field] = 0;
-                        ?>
-                    @endif
-                    <th style="text-align:center; vertical-align:middle; border:1px solid #000; padding:4px; background-color: #97d7f7;">{{ $dataTable->tableName }}</th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($data[0]->detail as $key => $dataTable)
-            <tr>
-                <td style="text-align:center; vertical-align:middle; border:1px solid #000;">{{ $key+1 }}</td>
-                @foreach($dataTable->field as $key2 => $dataTable2)
-                    <?php
-                    $alignment = "center";
-                    if($dataTable2->alignment == 1){
-                        $alignment = "left";
-                    }else if($dataTable2->alignment == 2){
-                        $alignment = "center";
-                    }else if($dataTable2->alignment == 3){
-                        $alignment = "right";
-                    }
-                    ?>
-                    @if(!empty($dataTable2->value) && !is_string($dataTable2->value) && $dataTable2->dataFormat == "#,##0")
-                        <?php
-                        $total[$dataTable2->field] += $dataTable2->value;
-                        ?>
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($dataTable2->value, 0, ',', '.') }}</td>
-                    @elseif(!empty($dataTable2->value) && !is_string($dataTable2->value) && $dataTable2->dataFormat == "#,##0.00")
-                        <?php
-                        $total[$dataTable2->field] += $dataTable2->value;
-                        ?>
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($dataTable2->value, 2, ',', '.') }}</td>
-                    @elseif(!empty($dataTable2->value) && $dataTable2->dataFormat == "dd/MM/YYYY")
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ date('d/m/Y', strtotime($dataTable2->value)) }}</td>
-                    @elseif(!empty($dataTable2->value) && $dataTable2->dataFormat == "dd MM YYYY")
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ date('d m Y', strtotime($dataTable2->value)) }}</td>
-                    @else
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ $dataTable2->value }}</td>
-                    @endif
-                @endforeach
-            </tr>
-            @endforeach
-            @if($grand_total)
-            <tr>
-                <td colspan="3" style="background-color: yellow; text-align:center; border:1px solid #000;">Grand Total</td>
-                @foreach($data[0]->detail[0]->field as $key3 => $dataTable3)
-                    <?php
-                    $alignment = "center";
-                    if($dataTable3->alignment == 1){
-                        $alignment = "left";
-                    }else if($dataTable3->alignment == 2){
-                        $alignment = "center";
-                    }else if($dataTable3->alignment == 3){
-                        $alignment = "right";
-                    }
-                    ?>
-                    @if($loop->iteration > 2)
-                        @if(!empty($total[$dataTable3->field]))
-                            @if(!is_string($dataTable3->value) && $dataTable3->dataFormat == "#,##0")
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($total[$dataTable3->field], 0, ',', '.') }}</td>
-                            @elseif(!is_string($dataTable3->value) && $dataTable3->dataFormat == "#,##0.00")
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($total[$dataTable3->field], 2, ',', '.') }}</td>
-                            @else
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">-</td>
-                            @endif
-                        @else
-                            <td style="text-align:{{ $alignment }}; border:1px solid #000;">-</td>
-                        @endif
-                    @endif
-                @endforeach
-            </tr>
+    @if(count($data) > 0 && (count($data[0]->detail) > 0))
+        <h3>
+            @if($company == 'NMDI' || $company == 'CITROEN')
+            {{ ($level1[0] == "ALL") ? $data_company[0]->companyName : 'PT ' . $data[0]->detail[0]->companyName }}
+            @else
+            {{ $data_company[0]->companyName }}
             @endif
         <br> 
             @if($company == 'NMDI' || $company == 'CITROEN')
--- resources/views/payroll/py_export_periodical_report_excel.blade.php
+++ resources/views/payroll/py_export_periodical_report_excel.blade.php
@@ -29,104 +29,16 @@
 	</style>
 </head>
 <body>
-    @if(count($data) > 0 && (count($data[0]->detail) > 0 || count($data[0]->summary) > 0))
-    <?php
-        $countcolspan = count($data[0]->detail[0]->field) + 3;
-    ?>
-    <table>
-        <thead>
-            <tr>
-                <th colspan="{{ $countcolspan }}">{{ $data_company[0]->companyName }}</th>
-            </tr>
-            <tr>
-                <th colspan="{{ $countcolspan }}">{{ $data_company[0]->address }}</th>
-            </tr>
-            <tr></tr>
-            <tr>
-                <th colspan="{{ $countcolspan }}" style="text-align:center; font-weight:bold;"><h3>{{ $report_name }}</h3></th>
-            </tr>
-            <tr>
-                <th colspan="{{ $countcolspan }}" style="text-align:center; font-weight:bold;"><pre>Periode   :    {{ $data_period }}</pre></th>
-            </tr>
-            <tr></tr>
-        </thead>
-    </table>
-    <?php
-    $total = [];
-    ?>
-    <table style="width: 100%;" class="table table-bordered table-hover responsive table_detail">
-        <thead>
-            <tr>
-                <th style="display:flex; text-align:center; align-items:center; border:1px solid #000; padding:4px; background-color: #97d7f7;">No</th>
-                @foreach($data[0]->detail[0]->field as $key => $dataTable)
-                    @if(!is_string($dataTable->value) && ($dataTable->dataFormat == "#,##0" || $dataTable->dataFormat == "#,##0.00"))
-                        <?php
-                        $total[$dataTable->field] = 0;
-                        ?>
-                    @endif
-                    <th style="text-align:center; vertical-align:middle; border:1px solid #000; padding:4px; background-color: #97d7f7;">{{ $dataTable->tableName }}</th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($data[0]->detail as $key => $dataTable)
-            <tr>
-                <td style="text-align:center; vertical-align:middle; border:1px solid #000;">{{ $key+1 }}</td>
-                @foreach($dataTable->field as $key2 => $dataTable2)
-                    <?php
-                    $alignment = "center";
-                    if($dataTable2->alignment == 1){
-                        $alignment = "left";
-                    }else if($dataTable2->alignment == 2){
-                        $alignment = "center";
-                    }else if($dataTable2->alignment == 3){
-                        $alignment = "right";
-                    }
-                    ?>
-                    @if(!empty($dataTable2->value) && !is_string($dataTable2->value) && $dataTable2->dataFormat == "#,##0")
-                        <?php
-                        $total[$dataTable2->field] += $dataTable2->value;
-                        ?>
-                        <td data-format="{{ $dataTable2->dataFormat }}" style="text-align:{{ $alignment }}; border:1px solid #000;">{{ $dataTable2->value }}</td>
-                    @elseif(!empty($dataTable2->value) && !is_string($dataTable2->value) && $dataTable2->dataFormat == "#,##0.00")
-                        <?php
-                        $total[$dataTable2->field] += $dataTable2->value;
-                        ?>
-                        <td data-format="{{ $dataTable2->dataFormat }}" style="text-align:right; border:1px solid #000;">{{ $dataTable2->value }}</td>
-                    
-                    @elseif(!empty($dataTable2->value) && $dataTable2->dataFormat == "dd/MM/YYYY")
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ date('d/m/Y', strtotime($dataTable2->value)) }}</td>
-                    @elseif(!empty($dataTable2->value) && $dataTable2->dataFormat == "dd MM YYYY")
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ date('d m Y', strtotime($dataTable2->value)) }}</td>
-                    @else
-                        <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ $dataTable2->value }}</td>
-                    @endif
-                @endforeach
-            </tr>
-            @endforeach
-            @if($grand_total)
-            <tr>
-                <td colspan="3" style="background-color: yellow; text-align:center; border:1px solid #000;">Grand Total</td>
-                @foreach($data[0]->detail[0]->field as $key3 => $dataTable3)
-                    <?php
-                    $alignment = "center";
-                    if($dataTable3->alignment == 1){
-                        $alignment = "left";
-                    }else if($dataTable3->alignment == 2){
-                        $alignment = "center";
-                    }else if($dataTable3->alignment == 3){
-                        $alignment = "right";
-                    }
-                    ?>
-                    @if($loop->iteration > 2)
-                        @if(!empty($total[$dataTable3->field]))
-                            @if(!is_string($dataTable3->value) && $dataTable3->dataFormat == "#,##0")
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($total[$dataTable3->field], 0, ',', '.') }}</td>
-                            @elseif(!is_string($dataTable3->value) && $dataTable3->dataFormat == "#,##0.00")
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">{{ number_format($total[$dataTable3->field], 2, ',', '.') }}</td>
-                            @else
-                                <td style="text-align:{{ $alignment }}; border:1px solid #000;">-</td>
-                            @endif
+    @if(count($data) > 0 && (count($data[0]->detail) > 0))
+        <?php
+            $countcolspan = count($data[0]->detail[0]->field) + 3;
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="{{ $countcolspan }}">
+                        @if($company == 'NMDI' || $company == 'CITROEN')
+                        {{ ($level1[0] == "ALL") ? $data_company[0]->companyName : 'PT ' . $data[0]->detail[0]->companyName }}
                         @else
                         {{ $data_company[0]->companyName }}
                         @endif
