5e9508f56c51fe3c0943b907e98e2e7f5ad89325
Yafie Imam <yafie.achmad@intikom.co.id>
2024-11-18 09:34:17 +0700

    Update Penyesuaian Penjagaan dan New Endpoint Update Absenteeism Data

--- app/Imports/UpdateAbsenteeismDataImport.php
+++ app/Imports/UpdateAbsenteeismDataImport.php
@@ -56,11 +56,11 @@ class UpdateAbsenteeismDataImport implements ToCollection, SkipsEmptyRows, WithS
             ]);
 
             Validator::make($rows->toArray(), [
-                '*.1' => 'nullable|date_format:m/d/Y',
+                '*.1' => 'nullable|date_format:Y-m-d',
                 '*.3' => 'nullable|date_format:H:i',
                 '*.4' => 'nullable|date_format:H:i'
             ], [
-                '*.1.date_format' => 'Date Format Must Be (01/31/2000)',
+                '*.1.date_format' => 'Date Format Must Be (2000-01-31). Make Sure to Change Column Format to Text, not Date',
                 '*.3.date_format' => 'Hour & Minute In Format Must Be (07:01). Make Sure to Change Column Format to Text, not Time',
                 '*.4.date_format' => 'Hour & Minute Out Format Must Be (07:01). Make Sure to Change Column Format to Text, not Time'
             ])->validate();
@@ -94,7 +94,7 @@ class UpdateAbsenteeismDataImport implements ToCollection, SkipsEmptyRows, WithS
 
             // dd(json_encode($param));
 
-            $response = $client->put(env('API_URL') . '/mobile/TmAbsentEmployee/BulkUpdateTmAbsentEmployee',
+            $response = $client->put(env('API_URL') . '/mobile/TmAbsentEmployee/UploadAbsentEmployee',
                 ['body' => json_encode($param)]
             );
         } catch (ValidationException $e) {
